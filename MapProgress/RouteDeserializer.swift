a7f5288c4b523bc78d384d5a8fc16eb54e75d431
James Robinson <github@epii.co.uk>
2015-07-21 23:45:18 +0100

    Started work on json deserialization

---
+++ MapProgress/RouteDeserializer.swift
@@ -0,0 +1,19 @@
+//
+//  RouteDeserializer.swift
+//  MapProgress
+//
+//  Created by James Robinson on 21/07/2015.
+//  Copyright (c) 2015 James Robinson. All rights reserved.
+//
+
+import Foundation
+
+class RouteDeserializer {
+    
+    init() {}
+    
+    func deserialize(data: NSString) -> Route {
+        return Route()
+    }
+    
+}
--- MapProgressTests/RouteCalculatorTests.swift
+++ MapProgressTests/RouteCalculatorTests.swift
@@ -11,21 +11,32 @@ import XCTest
 
 class RouteCalculatorTests: XCTestCase {
     
-    let routeManager = RouteManager()
-    let routeCalculator = RouteCalculator()
+    var routeManager: RouteManager?
+    var routeCalculator: RouteCalculator?
     
     override func setUp() {
-        super.setUp()
+        super.setUp();
+        let path = NSBundle(forClass: self.classForCoder).pathForResource("rockvale", ofType: "json")!;
+        let jsonData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!;
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary;
+        
+        routeManager = RouteManager(map: [NSString:Route]());
+        routeCalculator = RouteCalculator();
     }
     
     override func tearDown() {
         super.tearDown()
     }
     
-    func testDistance() {
-        var rockvale = routeManager.getRoute("Rockvale")
-        var altitude = routeCalculator.getAltitude(routeManager.getRoute("Rockvale"))
+    func testAltitude() {
+        var rockvale = routeManager!.getRoute("Rockvale")
+        var altitude = routeCalculator!.getAltitude(rockvale)
         XCTAssertEqual(rockvale.points.count, altitude.count, "The number of elements in the altitude list should be the same as the number in the original route")
     }
     
+    func testDistance() {
+        var rockvale = routeManager!.getRoute("Rockvale")
+        var distance = routeCalculator!.getDistance(rockvale)
+        XCTAssertEqual(rockvale.points.count, distance.count, "The number of elements in the distance list should be the same as the number in the original route")
+    }
 }
