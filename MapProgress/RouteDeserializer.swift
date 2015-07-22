2f6ddf428e5a26d71b29b7d0f327b79caf950efa
James Robinson <github@epii.co.uk>
2015-07-22 09:19:31 +0100

    Completed Route deserializer from NSData in JSON format

--- MapProgress/RouteDeserializer.swift
+++ MapProgress/RouteDeserializer.swift
@@ -12,8 +12,17 @@ class RouteDeserializer {
     
     init() {}
     
-    func deserialize(data: NSString) -> Route {
-        return Route()
+    func deserialize(data: NSData) -> Route {
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary;
+        var name : String = jsonResult["name"] as! String;
+        var points = [WayPoint]()
+        for point in (jsonResult["points"] as! NSArray) {
+            var wayPoint = point as! NSDictionary
+            points.append(WayPoint(northing: wayPoint["northing"] as! Double,
+                easting: wayPoint["easting"] as! Double,
+                altitude: wayPoint["altitude"] as! Double));
+        }
+        return Route(name: name, points: points)
     }
     
 }
