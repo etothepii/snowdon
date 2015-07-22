9d585c7fb4831806f922b5101532e44f39b404a1
James Robinson <github@epii.co.uk>
2015-07-22 09:20:51 +0100

    Rewrote tests to use deserialized data from json file in package

--- MapProgressTests/RouteCalculatorTests.swift
+++ MapProgressTests/RouteCalculatorTests.swift
@@ -11,17 +11,21 @@ import XCTest
 
 class RouteCalculatorTests: XCTestCase {
     
+    let routeDeserializer = RouteDeserializer()
     var routeManager: RouteManager?
     var routeCalculator: RouteCalculator?
     
     override func setUp() {
         super.setUp();
-        let path = NSBundle(forClass: self.classForCoder).pathForResource("rockvale", ofType: "json")!;
-        let jsonData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!;
-        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary;
-        
-        routeManager = RouteManager(map: [NSString:Route]());
+        routeManager = RouteManager();
         routeCalculator = RouteCalculator();
+        addRoute("rockvale");
+    }
+    
+    func addRoute(name: String) {
+        let path = NSBundle(forClass: self.classForCoder).pathForResource(name, ofType: "json")!;
+        let jsonData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!;
+        routeManager!.addRoute(routeDeserializer.deserialize(jsonData))
     }
     
     override func tearDown() {
