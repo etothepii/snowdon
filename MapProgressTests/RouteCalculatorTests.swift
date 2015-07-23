a1d64e82025faf9ec7dc0a658e15a524ca9f186d
James Robinson <github@epii.co.uk>
2015-07-23 08:40:37 +0100

    Added code to tests to load up results to test whetehr code works properly

--- MapProgressTests/RouteCalculatorTests.swift
+++ MapProgressTests/RouteCalculatorTests.swift
@@ -14,18 +14,36 @@ class RouteCalculatorTests: XCTestCase {
     let routeDeserializer = RouteDeserializer()
     var routeManager: RouteManager?
     var routeCalculator: RouteCalculator?
+    var altitude = [String:[Double]]()
+    var distance = [String:[Double]]()
     
     override func setUp() {
         super.setUp();
         routeManager = RouteManager();
         routeCalculator = RouteCalculator();
         addRoute("rockvale");
+        
     }
     
     func addRoute(name: String) {
-        let path = NSBundle(forClass: self.classForCoder).pathForResource(name, ofType: "json")!;
-        let jsonData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!;
+        var path = NSBundle(forClass: self.classForCoder).pathForResource(name, ofType: "json")!;
+        var jsonData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!;
         routeManager!.addRoute(routeDeserializer.deserialize(jsonData))
+        path = NSBundle(forClass: self.classForCoder).pathForResource(name + "_results", ofType: "json")!;
+        jsonData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!;
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary;
+        var routeAltitudes = [Double]()
+        for value in (jsonResult["altitude"] as! NSArray) {
+            var routeAltitude = value as! Double
+            routeAltitudes.append(routeAltitude)
+        }
+        altitude[name] = routeAltitudes
+        var routeDistances = [Double]()
+        for value in (jsonResult["distance"] as! NSArray) {
+            var routeDistance = value as! Double
+            routeDistances.append(routeDistance)
+        }
+        altitude[distance] = routeDistances
     }
     
     override func tearDown() {
