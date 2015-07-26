8dddc368eebeff890b39da2a38192ae71011de03
James Robinson <github@epii.co.uk>
2015-07-26 11:20:41 +0100

    Added translation to switch WGS84 cartesian to OSGB36 cartesian

--- MapProgress/CartesianPoint.swift
+++ MapProgress/CartesianPoint.swift
@@ -19,4 +19,24 @@ class CartesianPoint {
         self.y = y;
         self.z = z;
     }
+    
+    func toOSGB36() -> CartesianPoint {
+        
+        let tx = -446.448;
+        let ty =  125.157;
+        let tz = -542.060;
+        let rx =   -0.1502 / 3600 / 180 * M_PI;
+        let ry =   -0.2470 / 3600 / 180 * M_PI;
+        let rz =   -0.8421 / 3600 / 180 * M_PI;
+        let s  =   1 + 20.4894 / 1e6;
+        
+        // apply transform
+        let x2 = tx + x*s - y*rz + z*ry;
+        let y2 = ty + x*rz + y*s - z*rx;
+        let z2 = tz - x*ry + y*rx + z*s;
+        
+        var point = CartesianPoint(x: x2, y: y2, z: z2);
+        
+        return point;
+    }
 }
