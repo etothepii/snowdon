4f8b049bc9c57a7575f566b24bc1b8eb2a43287e
James Robinson <github@epii.co.uk>
2015-08-06 22:28:47 +0100

    Working chart ploting route on chart

--- MapProgress/MasterViewController.swift
+++ MapProgress/MasterViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class MasterViewController: UIViewController {
+class MasterViewController: UIViewController, CPTPlotDataSource {
     
     @IBOutlet var graphView: CPTGraphHostingView!
     
     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-
+    let routeCalculator = RouteCalculator()
+    
     var objects = [AnyObject]()
+    private var graph = CPTXYGraph(frame: CGRectZero)
+    private var data = [CGPoint]()
+    private var plotSpace: CPTXYPlotSpace? = nil
+    private var lines = [CPTScatterPlot]()
 
 
     override func awakeFromNib() {
@@ -24,12 +29,27 @@ class MasterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.whiteColor();
-        var graph = CPTXYGraph(frame: CGRectZero)
-        
+        plotSpace = graph.defaultPlotSpace as? CPTXYPlotSpace
+        var lineStyle = CPTMutableLineStyle()
+        lineStyle.miterLimit = CGFloat(1)
+        lineStyle.lineWidth = CGFloat(3)
+        lineStyle.lineColor = CPTColor.blueColor()
+        lines.append(CPTScatterPlot(frame: CGRectZero))
+        lines[0].dataLineStyle = lineStyle
+        lines[0].dataSource = self
+        graph.addPlot(lines[0], toPlotSpace: plotSpace!)
         graph.paddingLeft = 5
-        graph.paddingTop = 5
+        graph.paddingTop = 18
         graph.paddingRight = 5
         graph.paddingBottom = 5
+        
+        updateRoute(appDelegate.applicationContext.routeManager.getCurrentRoute())
+        
+        self.graphView.hostedGraph = graph
+    }
+    
+    private func updateRoute(route: Route) {
+        
         // Axes
         var axes = graph.axisSet as! CPTXYAxisSet
         var lineStyle = CPTMutableLineStyle()
@@ -37,7 +57,27 @@ class MasterViewController: UIViewController {
         axes.xAxis.axisLineStyle = lineStyle
         axes.yAxis.axisLineStyle = lineStyle
         
-        self.graphView.hostedGraph = graph
+        let altitude = routeCalculator.getAltitude(route)
+        let distance = routeCalculator.getDistance(route)
+        data = [CGPoint]()
+        for (var i = 0; i < altitude.count; i++) {
+            data.append(CGPoint(x: distance[i], y: altitude[i]))
+        }
+        plotSpace!.scaleToFitPlots(lines)
+    }
+
+    func numberOfRecordsForPlot(plot: CPTPlot!) -> UInt {
+        return UInt(data.count)
+    }
+    
+    func doubleForPlot(plot: CPTPlot!, field: UInt, recordIndex: UInt) -> Double {
+        var datum: CGPoint = data[Int(recordIndex)]
+        switch (field) {
+            case 0:
+                return Double(datum.x)
+            default:
+                return Double(datum.y)
+        }
     }
     
     override func didReceiveMemoryWarning() {
