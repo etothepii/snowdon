5f4c1fee39cdda8871ce5dc85de26bb7d447fca0
James Robinson <github@epii.co.uk>
2015-08-12 12:03:09 +0100

    Linked the route name label to the view model and linked the newly created table view comtroller to the cells and the route manager and placed the table view at the start of the application

---
+++ MapProgress/RouteTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  RouteTableViewCell.swift
+//  
+//
+//  Created by James Robinson on 12/08/2015.
+//
+//
+
+import UIKit
+
+class RouteTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var RouteName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
